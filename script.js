f7ca55743c13a0795eb7251dc92605e7cbb0e0f3
jonathan-epc <jonathan.e.poblete.c@gmail.com>
2024-09-16 19:17:46 -0300

    Added UI

--- script.js
+++ script.js
@@ -9,52 +9,62 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  let choice = prompt("Rock, Paper, Scissors?:").toLowerCase();
-  return choice;
-}
-
 function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
   if (humanChoice == computerChoice) {
-    console.log("Tie");
-    return;
   } else if (humanChoice == "rock") {
     if (computerChoice == "scissors") {
-      humanScore += 1;
-      console.log("Human wins!");
+      addPoint(humanScore);
     } else {
-      computerScore += 1;
-      console.log("Computer wins!");
+      addPoint(computerScore);
     }
   } else if (humanChoice == "paper") {
     if (computerChoice == "rock") {
-      humanScore += 1;
-      console.log("Human wins!");
+      addPoint(humanScore);
     } else {
-      computerScore += 1;
-      console.log("Computer wins!");
+      addPoint(computerScore);
     }
   } else {
     if (computerChoice == "paper") {
-      humanScore += 1;
-      console.log("Human wins!");
+      addPoint(humanScore);
     } else {
-      computerScore += 1;
-      console.log("Computer wins!");
+      addPoint(computerScore);
+    }
+  }
+  checkEnd();
+}
+
+function checkEnd() {
+  let computerScoreNumber = parseInt(computerScore.textContent);
+  let humanScoreNumber = parseInt(humanScore.textContent);
+  if (computerScoreNumber >= 5 || humanScoreNumber >= 5) {
+    if (computerScoreNumber > humanScoreNumber) {
+      results.textContent = "Computer wins!";
+    } else if (humanScoreNumber > computerScoreNumber) {
+      results.textContent = "Human wins!";
+    } else {
+      results.textContent = "Tie!";
     }
   }
 }
 
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-    console.log("Human score:" + humanScore);
-    console.log("Computer score:" + computerScore);
-  }
+function addPoint(score) {
+  score.textContent = parseInt(score.textContent) + 1;
 }
 
-var humanScore = 0;
-var computerScore = 0;
-playGame();
+var results = document.querySelector("#results");
+var buttons = document.querySelectorAll("button");
+var humanScore = document.querySelector("#human_score");
+var computerScore = document.querySelector("#computer_score");
+var humanChoiceVis = document.querySelector("#span_human_choice");
+var computerChoiceVis = document.querySelector("#span_computer_choice");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    let humanChoice = button.textContent;
+    humanChoiceVis.textContent = humanChoice;
+    let computerChoice = getComputerChoice();
+    computerChoiceVis.textContent = computerChoice;
+    playRound(humanChoice, computerChoice);
+  });
+});
